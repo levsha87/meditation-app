be68287c86eff8e9ca27adf0417dc4d4e4a2e674
Roma Levsha <rm.levsha@gmail.com>
2020-10-23 12:33:54 +0300

    feat: add function +10s

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 const song = document.querySelector(".song"),
-        video = document.querySelector(".vid-container video"),
-        play = document.querySelector(".play"),
-        replay = document.querySelector(".replay"),
-        outline = document.querySelector(".moving-outline circle");
+  video = document.querySelector(".vid-container video"),
+  play = document.querySelector(".play"),
+  replay = document.querySelector(".replay"),
+  outline = document.querySelector(".moving-outline circle"),
+  arrowRight = document.querySelector(".arrow-right");
+console.log(arrowRight);
 
 //Sounds
 const sounds = document.querySelectorAll(".sound-picker button");
@@ -18,39 +20,41 @@ let timeDuration = 600;
 outline.style.strokeDashoffset = outlineLength;
 outline.style.strokeDasharray = outlineLength;
 
-timeDisplay.textContent = `${Math.floor(timeDuration / 60)}:${plusZero(Math.floor(
-    timeDuration % 60)
-  )}`;
+timeDisplay.textContent = `${Math.floor(timeDuration / 60)}:${plusZero(
+  Math.floor(timeDuration % 60)
+)}`;
 
-sounds.forEach(sound => {
-  sound.addEventListener("click", function(){
+sounds.forEach((sound) => {
+  sound.addEventListener("click", function () {
     song.src = this.getAttribute("data-sound");
     video.src = this.getAttribute("data-video");
     checkPlaying(song);
   });
 });
 
-play.addEventListener("click", function(){
+play.addEventListener("click", function () {
   checkPlaying(song);
 });
 
-replay.addEventListener("click", function(){
+replay.addEventListener("click", function () {
   restartSong(song);
 });
 
-const restartSong = song => {
+const restartSong = (song) => {
   song.currentTime = 0;
 };
 
-timeSelect.forEach(valueTime => {
-  valueTime.addEventListener("click", function(){
+timeSelect.forEach((valueTime) => {
+  valueTime.addEventListener("click", function () {
     timeDuration = this.getAttribute("data-time");
-    timeDisplay.textContent = `${Math.floor(timeDuration / 60)}:${plusZero(Math.floor(timeDuration % 60))}`;
+    timeDisplay.textContent = `${Math.floor(timeDuration / 60)}:${plusZero(
+      Math.floor(timeDuration % 60)
+    )}`;
   });
 });
 
-const checkPlaying = song => {
-  if (song.paused){
+const checkPlaying = (song) => {
+  if (song.paused) {
     song.play();
     video.play();
     play.src = "./svg/pause.svg";
@@ -61,25 +65,43 @@ const checkPlaying = song => {
   }
 };
 
-function plusZero (n) {
+function plusZero(n) {
   return (parseInt(n, 10) < 10 ? "0" : "") + n;
 }
 
-song.ontimeupdate  = function () {
-  let currentTime = song.currentTime;
-  let timePassed = timeDuration - currentTime;
-  let seconds = plusZero(Math.floor(timePassed % 60));
+song.ontimeupdate = function () {
+  getCurrentTime();
+  displayProgress();
 
-  let minuts = Math.floor(timePassed / 60);
-  timeDisplay.textContent  = `${minuts}:${seconds}`;
-  let progress = outlineLength * (1- currentTime/timeDuration);
-  outline.style.strokeDashoffset = progress;
-
-  if(currentTime >= timeDuration){
+  if (currentTime >= timeDuration) {
     song.pause();
     song.currentTime = 0;
-    play.src = "./svg/play.svg"
+    play.src = "./svg/play.svg";
     video.pause();
   }
 };
 
+function getCurrentTime() {
+  currentTime = song.currentTime;
+}
+function displayProgress() {
+  let timePassed = timeDuration - currentTime;
+  let seconds = plusZero(Math.floor(timePassed % 60));
+  let minuts = Math.floor(timePassed / 60);
+  timeDisplay.textContent = `${minuts}:${seconds}`;
+  let progress = outlineLength * (1 - currentTime / timeDuration);
+  outline.style.strokeDashoffset = progress;
+}
+
+arrowRight.addEventListener("click", function () {
+  if (timeDuration - currentTime < 10) {
+    song.currentTime = timeDuration;
+    currentTime = song.currentTime;
+    displayProgress(currentTime);
+    getCurrentTime(currentTime);
+
+  } else {
+    song.currentTime += 10;
+    currentTime = song.currentTime;
+  }
+});
